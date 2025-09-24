06cb51b135b1eac92f7717ed8e0f72d9550ddd70
Ashish-Sirobilt <ashish.kumar@sirobilt.com>
2025-09-24 17:19:26 +0530

    Welcome route for local-server

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ app.use(cors());
 
 const PORT = 3001;
 
+app.get('/', (req, res) => {
+    res.status(200).send({ success: true, message: "Hurray !!! Welcome to Local-Server" })
+})
+
 app.listen(PORT, () => console.log(`Local Server started at PORT ${PORT}`));
